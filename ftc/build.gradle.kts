0962ea715e04df24e0c13c29a42d53b79a4c96b1
Sidd Rao <siddrao.926@gmail.com>
2023-11-15 14:44:51 -0800

    Added FTC Dependencies To FTC Module

--- ftc/build.gradle.kts
+++ ftc/build.gradle.kts
@@ -28,7 +28,22 @@ android {
 dependencies {
     implementation(project(":core"))
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    // TODO: Decide Which FTC Dependencies To Keep
+    implementation ("org.firstinspires.ftc:Inspection:9.0.1")
+    implementation ("org.firstinspires.ftc:Blocks:9.0.1")
+    implementation ("org.firstinspires.ftc:Tfod:9.0.1")
+    implementation ("org.firstinspires.ftc:RobotCore:9.0.1")
+    implementation ("org.firstinspires.ftc:RobotServer:9.0.1")
+    implementation ("org.firstinspires.ftc:OnBotJava:9.0.1")
+    implementation ("org.firstinspires.ftc:Hardware:9.0.1")
+    implementation ("org.firstinspires.ftc:FtcCommon:9.0.1")
+    implementation ("org.firstinspires.ftc:Vision:9.0.1")
+    implementation ("org.firstinspires.ftc:gameAssets-CenterStage:1.0.0")
+    implementation ("org.tensorflow:tensorflow-lite-task-vision:0.4.3")
+    runtimeOnly ("org.tensorflow:tensorflow-lite:2.12.0")
+    implementation ("androidx.appcompat:appcompat:1.6.1")
+
+    implementation ("org.ftclib.ftclib:core:2.1.1") // core
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("com.android.support.test:runner:1.0.2")
